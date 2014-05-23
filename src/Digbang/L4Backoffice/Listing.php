7b378a68ea40cff928f2106dcb07a80f1562b28a
gcontreras <gcontreras@digbang.com>
2014-05-23 16:04:27 -0300

    Listing changes

--- src/Digbang/L4Backoffice/Listing.php
+++ src/Digbang/L4Backoffice/Listing.php
@@ -3,11 +3,76 @@
 namespace Digbang\L4Backoffice;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Contracts\ArrayableInterface;
+use Illuminate\Support\Contracts\RenderableInterface;
+use Digbang\L4Backoffice\Filters\Collection as FilterCollection;
+use Illuminate\View\Environment as ViewFactory;
 
-class Listing extends Collection
+class Listing extends Collection implements RenderableInterface
 {
-	public function __construct(ColumnCollection $columnCollection)
+	protected $view = 'l4-backoffice::listing';
+
+	/**
+	 * @var \Illuminate\View\Environment
+	 */
+	protected $viewFactory;
+
+	protected $filters;
+
+	function __construct(ViewFactory $viewFactory, FilterCollection $filters)
 	{
-		$this->merge($columnCollection);
+		$this->viewFactory = $viewFactory;
+		$this->filters = $filters;
+	}
+
+	/**
+	 * @param string $view
+	 */
+	public function setView($view)
+	{
+		$this->view = $view;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getView()
+	{
+		return $this->view;
+	}
+
+	/**
+	 * @param ColumnCollection $columnCollection
+	 * @return Listing $this
+	 */
+	public function columns(ColumnCollection $columnCollection)
+	{
+		return $this->mergeInto($columnCollection);
+	}
+
+	public function filters()
+	{
+
+	}
+
+	public function mergeInto($items)
+	{
+		if ($items instanceof Collection)
+		{
+			$items = $items->all();
+		}
+		elseif ($items instanceof ArrayableInterface)
+		{
+			$items = $items->toArray();
+		}
+
+		$this->items = array_merge($this->items, $items);
+
+		return $this;
+	}
+
+	public function render()
+	{
+		return $this->viewFactory->make($this->view, $this->toArray())->render();
 	}
 }
--- tests/spec/Digbang/L4Backoffice/ListingSpec.php
+++ tests/spec/Digbang/L4Backoffice/ListingSpec.php
@@ -2,7 +2,9 @@
 
 namespace spec\Digbang\L4Backoffice;
 
-use Digbang\L4Backoffice\ColumnCollection;
+use Digbang\L4Backoffice\Filters\Collection as FilterCollection;
+use Illuminate\View\Environment;
+use Illuminate\View\View;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -12,16 +14,20 @@ use Prophecy\Argument;
  */
 class ListingSpec extends ObjectBehavior
 {
-	function let(ColumnCollection $columnCollection)
-	{
-		$columnCollection->toArray()->willReturn(array());
+	private $message = 'I render thee';
+	private $viewFactory;
 
-		$this->beConstructedWith($columnCollection);
+	function let(Environment $viewFactory, FilterCollection $filterCollection)
+	{
+		$this->viewFactory = $viewFactory;
+
+		$this->beConstructedWith($this->viewFactory, $filterCollection);
 	}
 
     function it_is_initializable()
     {
         $this->shouldHaveType('Digbang\L4Backoffice\Listing');
+	    $this->shouldHaveType('Illuminate\Support\Contracts\RenderableInterface');
     }
 
 	function it_should_be_a_collection()
@@ -29,5 +35,14 @@ class ListingSpec extends ObjectBehavior
 		$this->shouldHaveType('Illuminate\\Support\\Collection');
 	}
 
+	function it_should_be_renderable(View $view)
+	{
+		// Mock the view factory's make method
+		$this->viewFactory->make($this->getView(), $this->toArray())->willReturn($view);
 
+		// And mock the view render method
+		$view->render()->willReturn($this->message);
+
+		$this->render()->shouldReturn($this->message);
+	}
 }
