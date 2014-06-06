b27df2a2b46e304ce1557c3c64cd3edf36e46b4d
gcontreras <gcontreras@digbang.com>
2014-06-06 13:27:24 -0300

    Empty template

---
+++ src/views/empty.blade.php
@@ -0,0 +1,14 @@
+@extends('l4-backoffice::layouts.default')
+
+@if(isset($title))
+	@section('head.title', $title)
+@endif
+
+@if(isset($breadcrumb))
+	@section('titlebar.breadcrumb', $breadcrumb)
+@endif
+
+@if(isset($actions))
+	@section('titlebar.actions')
+	@stop
+@endif
