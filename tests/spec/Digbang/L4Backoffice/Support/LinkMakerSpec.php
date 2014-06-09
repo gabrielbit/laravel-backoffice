5f0e35793734103916cf49a98e46a11e99e7b3d4
gcontreras <gcontreras@digbang.com>
2014-06-09 11:49:19 -0300

    Mocking request class in LinkMaker spec

--- tests/spec/Digbang/L4Backoffice/Support/LinkMakerSpec.php
+++ tests/spec/Digbang/L4Backoffice/Support/LinkMakerSpec.php
@@ -1,10 +1,16 @@
 <?php namespace spec\Digbang\L4Backoffice\Support;
 
+use Illuminate\Http\Request;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class LinkMakerSpec extends ObjectBehavior
 {
+	function let(Request $request)
+	{
+		$this->beConstructedWith($request);
+	}
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Digbang\L4Backoffice\Support\LinkMaker');
