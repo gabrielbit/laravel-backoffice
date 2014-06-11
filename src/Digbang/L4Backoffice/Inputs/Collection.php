bb97a7a2cefd18a7e4c629321945458bf25005ea
gcontreras <gcontreras@digbang.com>
2014-06-11 16:49:15 -0300

    Refactored some variables that were still called "filter"

--- src/Digbang/L4Backoffice/Inputs/Collection.php
+++ src/Digbang/L4Backoffice/Inputs/Collection.php
@@ -23,14 +23,14 @@ class Collection implements \IteratorAggregate
 
 	public function find($name)
 	{
-		return $this->collection->first(function($key, InputInterface $filter) use ($name){
-			return $filter->name() == $name;
+		return $this->collection->first(function($key, InputInterface $input) use ($name){
+			return $input->name() == $name;
 		});
 	}
 
-	public function add(InputInterface $filter)
+	public function add(InputInterface $input)
 	{
-		$this->collection->push($filter);
+		$this->collection->push($input);
 
 		return $this;
 	}
@@ -51,7 +51,6 @@ class Collection implements \IteratorAggregate
 	}
 
 	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
 	 * Retrieve an external iterator
 	 * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
 	 * @return \Traversable An instance of an object implementing <b>Iterator</b> or
@@ -61,6 +60,4 @@ class Collection implements \IteratorAggregate
 	{
 		return $this->collection;
 	}
-
-
 }
