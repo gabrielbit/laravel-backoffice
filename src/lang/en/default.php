ed4e79846db00e14391a864d3bf21291b73c6a2b
gcontreras <gcontreras@digbang.com>
2014-06-11 17:03:20 -0300

    Added text for "new item"

--- src/lang/en/default.php
+++ src/lang/en/default.php
@@ -9,5 +9,6 @@ return [
 	'actions'          => 'Actions',
 	'empty_listing'    => 'There are no items.',
 	'select-all'       => 'Select all',
-	'select-none'      => 'Select none'
+	'select-none'      => 'Select none',
+	'new'              => 'New :model'
 ];
