5d4d9ad0953819d37bd0d82df125d614396f1f34
gcontreras <gcontreras@digbang.com>
2014-08-13 15:28:34 -0300

    Removed comment block

--- src/Digbang/L4Backoffice/Generator/Services/ControllerGenerator.php
+++ src/Digbang/L4Backoffice/Generator/Services/ControllerGenerator.php
@@ -33,24 +33,6 @@ class ControllerGenerator
 				$entityNamespace . $className,
 				$columns
 			)
-		/*
-			[
-				'namespace'              => $namespace,
-				'classname'              => $className,
-				'snake_classname'        => $tableName,
-				'plural_classname'       => $pluralClassname,
-				'camel_classname'        => \Str::camel($tableName),
-				'full_model'             => $entityNamespace . $className,
-				'title_attribute_getter' => array_first($columns, function($key, $value){ return $value != 'id'; }),
-				'inputs_into_columns'    => $this->columnsInputs($columns),
-				'data_into_columns'      => $this->columnsData($columns),
-				'columns'                => $columns,
-				'columns_with_labels'    => $this->columnsLabel($columns),
-				'columns_hide'           => in_array('id', $columns) ? "'id'" : '',
-				'columns_sortable'       => $this->columns($columns),
-				'form_inputs'            => $this->formInputs($editableColumns),
-				'filters'                => $this->filters($columns)
-			]*/
 		);
 	}
 }
