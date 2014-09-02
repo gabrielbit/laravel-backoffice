273f5d0c1293bd109b1549c858583629ad3d151f
gcontreras <gcontreras@digbang.com>
2014-09-02 12:39:18 -0300

    Improved checkboxes in forms

--- src/views/inputs/checkbox.blade.php
+++ src/views/inputs/checkbox.blade.php
@@ -1,5 +1,5 @@
 <div class="checkbox">
 	<label for="{{ $name }}"{{ HTML::attributes(array_diff_key($options, ['id' => 'id'])) }}>
-		{{ Form::checkbox($name, 'On', $value, ['id' => $options['id']]) }}
+		{{ Form::checkbox($name, 'On', $value, ['id' => $options['id']]) }} &nbsp;
 	</label>
 </div>
