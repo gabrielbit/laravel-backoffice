1da1f15766d062c7a8612994f5e1fb69772f36f6
Guido Contreras Woda <gcontreras@digbang.com>
2014-09-15 13:21:18 -0300

    Always show the full listing. Show the "no items" message inside the listing table.

--- src/views/listing.blade.php
+++ src/views/listing.blade.php
@@ -1,26 +1,20 @@
-@if(count($items))
-	@include('l4-backoffice::filters', ['filters' => $filters])
-	<div class="row">
-	    <div class="col-sm-12 col-md-12">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<div class="header-list">
-						@if($actions || $bulkActions)
-							@include('l4-backoffice::lists.actions', ['actions' => $actions, 'bulkActions' => $bulkActions])
-						@endif
-						@if($paginator)
-							@include('l4-backoffice::lists.pagination', ['paginator' => $paginator])
-						@endif
-					</div>
-					<div class="results-list">
-						@include('l4-backoffice::lists.list', ['bulkActions' => $bulkActions, 'columns' => $columns, 'rowActions' => $rowActions, 'items' => $items])
-					</div>
+@include('l4-backoffice::filters', ['filters' => $filters])
+<div class="row">
+	<div class="col-sm-12 col-md-12">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<div class="header-list">
+					@if($actions || $bulkActions)
+						@include('l4-backoffice::lists.actions', ['actions' => $actions, 'bulkActions' => $bulkActions])
+					@endif
+					@if($paginator)
+						@include('l4-backoffice::lists.pagination', ['paginator' => $paginator])
+					@endif
+				</div>
+				<div class="results-list">
+					@include('l4-backoffice::lists.list', ['bulkActions' => $bulkActions, 'columns' => $columns, 'rowActions' => $rowActions, 'items' => $items])
 				</div>
 			</div>
 		</div>
 	</div>
-@else
-	<div class="alert alert-danger">
-		{{ Lang::get('l4-backoffice::default.empty_listing') }}
-	</div>
-@endif
+</div>
--- src/views/lists/list.blade.php
+++ src/views/lists/list.blade.php
@@ -1,44 +1,52 @@
 <table class="table table-striped table-bordered">
 	<thead>
-		<tr>
-			@if(count($bulkActions))
-				<th class="selectors">
-					{{ Form::checkbox('all', 'all', null, ['class' => 'chk-all']) }}
-				</th>
-			@endif
-			@foreach($columns as $column)
-				<th>
-					@if($column->sortable())
-						@include('l4-backoffice::actions.sort', ['column' => $column])
-					@else
-						{{ $column->getLabel() }}
-					@endif
-				</th>
-			@endforeach
-			@if($rowActions)
-				<th>{{ Lang::get('l4-backoffice::default.actions') }}</th>
-			@endif
-		</tr>
-	</thead>
-	<tbody>
-	@foreach($items as $row)
-		<tr>
+	<tr>
 		@if(count($bulkActions))
-			<td>
-				{{ Form::checkbox('row', array_get($row, 'id'), null, ['class' => 'chk-bulk']) }}
-			</td>
+		<th class="selectors">
+			{{ Form::checkbox('all', 'all', null, ['class' => 'chk-all']) }}
+		</th>
 		@endif
 		@foreach($columns as $column)
-			<td>{{ \Str::parse($column->getValue($row)) ?: '-' }}</td>
+		<th>
+			@if($column->sortable())
+				@include('l4-backoffice::actions.sort', ['column' => $column])
+			@else
+				{{ $column->getLabel() }}
+			@endif
+		</th>
 		@endforeach
 		@if($rowActions)
-			<td class="row-actions">
-				@foreach($rowActions as $action)
-					{{ $action->renderWith($row) }}
-				@endforeach
-			</td>
+		<th>{{ Lang::get('l4-backoffice::default.actions') }}</th>
 		@endif
-		</tr>
+	</tr>
+	</thead>
+	<tbody>
+	@if(count($items))
+	@foreach($items as $row)
+	<tr>
+		@if(count($bulkActions))
+		<td>
+			{{ Form::checkbox('row', array_get($row, 'id'), null, ['class' => 'chk-bulk']) }}
+		</td>
+		@endif
+		@foreach($columns as $column)
+		<td>{{ \Str::parse($column->getValue($row)) ?: '-' }}</td>
+		@endforeach
+		@if($rowActions)
+		<td class="row-actions">
+			@foreach($rowActions as $action)
+				{{ $action->renderWith($row) }}
+			@endforeach
+		</td>
+		@endif
+	</tr>
 	@endforeach
+	@else
+	<tr>
+		<td colspan="{{ count($columns) + 1 }}" class="text-danger">
+			{{ Lang::get('l4-backoffice::default.empty_listing') }}
+		</td>
+	</tr>
+	@endif
 	</tbody>
 </table>
