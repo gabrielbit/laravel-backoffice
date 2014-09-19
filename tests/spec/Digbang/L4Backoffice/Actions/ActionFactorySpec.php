953e1a52d7ea1991a79a3e741fc1434537afcb1a
Guido Contreras Woda <gcontreras@digbang.com>
2014-09-19 13:05:40 -0300

    Forgot to commit tests

--- tests/spec/Digbang/L4Backoffice/Actions/ActionFactorySpec.php
+++ tests/spec/Digbang/L4Backoffice/Actions/ActionFactorySpec.php
@@ -1,10 +1,19 @@
 <?php namespace spec\Digbang\L4Backoffice\Actions;
 
 use Digbang\L4Backoffice\Controls\ControlFactory;
+use Digbang\L4Backoffice\Forms\Form;
+use Digbang\L4Backoffice\Forms\FormFactory;
+use Digbang\L4Backoffice\Inputs\InputFactory;
 use Illuminate\View\Factory;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ * Class ActionFactorySpec
+ *
+ * @package spec\Digbang\L4Backoffice\Actions
+ * @mixin \Digbang\L4Backoffice\Actions\ActionFactory
+ */
 class ActionFactorySpec extends ObjectBehavior
 {
 	function let(Factory $viewFactory)
@@ -28,5 +37,17 @@ class ActionFactorySpec extends ObjectBehavior
 	{
 		$this->form('/', 'Press my buttons!')->shouldBeAnInstanceOf('Digbang\L4Backoffice\Actions\Action');
 		$this->form('/', 'Press my buttons!', ['class' => 'btn btn-default'])->shouldBeAnInstanceOf('Digbang\L4Backoffice\Actions\Action');
+	}
+
+	function it_should_create_modal_actions_with_a_form(Form $form)
+	{
+		$this->modal($form, 'aLabel', ['some' => 'options'], 'anIcon')->shouldBeAnInstanceOf('Digbang\L4Backoffice\Actions\Modal');
+	}
+
+	function it_should_create_modal_actions_with_a_closure(Form $form)
+	{
+		$this->modal(function($row){
+				return $form;
+			}, 'aLabel', ['some' => 'options'], 'anIcon')->shouldBeAnInstanceOf('Digbang\L4Backoffice\Actions\Modal');
 	}
 }
--- tests/spec/Digbang/L4Backoffice/Actions/ModalSpec.php
+++ tests/spec/Digbang/L4Backoffice/Actions/ModalSpec.php
@@ -2,6 +2,7 @@
 
 use Digbang\L4Backoffice\Controls\ControlInterface;
 use Digbang\L4Backoffice\Forms\Form;
+use Illuminate\Support\Collection;
 use Illuminate\View\View;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -24,5 +25,10 @@ class ModalSpec extends ObjectBehavior
 	function it_should_be_renderable()
 	{
 		$this->render()->shouldBeAnInstanceOf('Illuminate\View\View');
+	}
+
+	function it_should_be_renderable_with_a_row()
+	{
+		$this->renderWith(new Collection(['some' => 'items']))->shouldBeAnInstanceOf('Illuminate\View\View');
 	}
 }
