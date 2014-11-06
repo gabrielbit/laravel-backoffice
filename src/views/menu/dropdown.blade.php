00b79e5fc18d8f13f45eaeb6a03237d2dfd2c6a8
Guido Contreras Woda <gcontreras@digbang.com>
2014-11-06 10:02:37 -0300

    Fixes #17: If the dropdown has no actions (may be because of permissions or wrong configuration), don't show it

--- src/views/menu/dropdown.blade.php
+++ src/views/menu/dropdown.blade.php
@@ -1,2 +1,4 @@
-@include('l4-backoffice::menu.link', ['target' => $target, 'label' => $label, 'icon' => $icon, 'options' => []])
-@include('l4-backoffice::menu.list', ['actionTree' => $actions, 'options' => ['class' => 'children', 'style' => ($isActive ? 'display: block' : '')]])
+@if ($actions->count() > 0)
+	@include('l4-backoffice::menu.link', ['target' => $target, 'label' => $label, 'icon' => $icon, 'options' => []])
+	@include('l4-backoffice::menu.list', ['actionTree' => $actions, 'options' => ['class' => 'children', 'style' => ($isActive ? 'display: block' : '')]])
+@endif
